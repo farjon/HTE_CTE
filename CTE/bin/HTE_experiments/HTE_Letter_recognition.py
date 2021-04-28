0449226005eeb1c4eb0e43b7ea4f561d418b3020
farjon <guyfar@post.bgu.ac.il>
2021-04-28 11:53:31 +0300

    Letter_recognition script
    updating the letter recognition
    this is the most recent training script update
    HTE_general should be changed accordingly

--- CTE/bin/HTE_experiments/HTE_Letter_recognition.py
+++ CTE/bin/HTE_experiments/HTE_Letter_recognition.py
@@ -7,12 +7,13 @@ import torch
 import torch.nn as nn
 from CTE.utils.datasets import Letter_dataset
 from torch import optim
-from CTE.utils.help_funcs import save_anneal_params, load_anneal_params
+from CTE.utils.help_funcs import save_anneal_params, load_anneal_params, print_end_experiment_report
 from CTE.bin.HTE_experiments.training_functions import train_loop
 from CTE.utils.datasets.create_letters_dataset import main as create_letters_dataset
 
 def main():
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
+    # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     if device.type == 'cuda':
         torch.cuda.set_device(0)
         torch.backends.cudnn.deterministic = True
@@ -31,7 +32,7 @@ def main():
 
     # path to save models
     experiment_name = 'HTE-Letter-Recognition'
-    experiment_number = '10'
+    experiment_number = '2'
     args.save_path = os.path.join(GetEnvVar('ModelsPath'), 'Guy', 'HTE_pytorch', experiment_name, experiment_number)
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
@@ -39,11 +40,11 @@ def main():
                                         experiment_number)
 
     # optimization Parameters
-    args.word_calc_learning_rate = 0.001
+    args.word_calc_learning_rate = 0.01
     args.voting_table_learning_rate = 0.1
 
     args.LR_decay = 0.999
-    args.num_of_epochs = 50
+    args.num_of_epochs = 60
     args.batch_size = 200
     args.optimizer = 'ADAM'
     args.loss = 'categorical_crossentropy'
@@ -72,6 +73,7 @@ def main():
 
     # Letter recognition dataset has 16 features
     D_in = 16
+    D_out_1 = 12
     D_out = 26
     args.input_size = [args.batch_size, D_in]
     # Decide on the ferns parameters and sparse table parameters
@@ -82,10 +84,12 @@ def main():
     # Sparse Table should include:
     #   D_out - number of features for next layer
     args.Fern_layer = [
-        {'K': 7, 'M': 20, 'num_of_features': D_in}
+        {'K': 6, 'M': 100, 'num_of_features': D_in},
+        {'K': 5, 'M': 50, 'num_of_features': D_out_1}
     ]
     args.ST_layer = [
-        {'Num_of_active_words': 2**args.Fern_layer[0]['K'], 'D_out': D_out}
+        {'Num_of_active_words': 2**args.Fern_layer[0]['K'], 'D_out': D_out_1},
+        {'Num_of_active_words': 2**args.Fern_layer[1]['K'], 'D_out': D_out}
     ]
 
     args.prune_type = 1
@@ -96,7 +100,8 @@ def main():
 
     model = HTE(args, args.input_size, device)
 
-    voting_table_LR_params_list = ['voting_table_layers.0.weights', 'voting_table.layers.0.bias']
+    voting_table_LR_params_list = ['voting_table_layers.0.weights', 'voting_table.layers.0.bias',
+                                   'voting_table_layers.1.weights', 'voting_table.layers.1.bias']
     voting_table_params = list(map(lambda x: x[1], list(filter(lambda kv: kv[0] in voting_table_LR_params_list, model.named_parameters()))))
     word_calc_params = list(map(lambda x: x[1], list(filter(lambda kv: kv[0] not in voting_table_LR_params_list, model.named_parameters()))))
 
@@ -107,22 +112,24 @@ def main():
     saving_path = os.path.join(args.save_path)
 
     paths_to_save_anneal_params = []
-    paths_to_save_anneal_params.append(os.path.join(saving_path, 'ambiguity_thresholds_layer_1.p'))
-    paths_to_save_anneal_params.append(os.path.join(saving_path, 'anneal_params_1.p'))
+    for i in range(0,args.number_of_layers*2,2):
+        paths_to_save_anneal_params.append(os.path.join(saving_path, 'ambiguity_thresholds_layer_'+str(i)+'.p'))
+        paths_to_save_anneal_params.append(os.path.join(saving_path, 'anneal_params_'+str(i+1)+'.p'))
     args.paths_to_save = paths_to_save_anneal_params
 
     def save_model_anneal_params(model, paths_to_save):
-        path_to_AT = paths_to_save[0]
-        path_to_anneal_params = paths_to_save[1]
-        save_anneal_params(model.word_calc_layers[0], path_to_AT, path_to_anneal_params)
+        for i in range(0, args.number_of_layers*2,2):
+            path_to_AT = paths_to_save[i]
+            path_to_anneal_params = paths_to_save[i+1]
+            save_anneal_params(model.word_calc_layers[int(i/2)], path_to_AT, path_to_anneal_params)
 
     def load_model_anneal_params(model, paths_to_save):
-        path_to_AT = paths_to_save[0]
-        path_to_anneal_params = paths_to_save[1]
-        # AT - ambiguity threshold, AP - anneal params
-        AT, AP = load_anneal_params(path_to_AT, path_to_anneal_params)
-        model.word_calc_layers[0].ambiguity_thresholds = AT
-        model.word_calc_layers[0].anneal_state_params = AP
+        for i in range(0, args.number_of_layers*2,2):
+            path_to_AT = paths_to_save[i]
+            path_to_anneal_params = paths_to_save[i+1]
+            AT, AP = load_anneal_params(path_to_AT, path_to_anneal_params)
+            model.word_calc_layers[int(i/2)].ambiguity_thresholds = AT
+            model.word_calc_layers[int(i/2)].anneal_state_params = AP
         return model
 
     if not os.path.exists(saving_path):
@@ -132,10 +139,38 @@ def main():
 
     #save model and ambiguity_thresholds
     torch.save(final_model.state_dict(), os.path.join(saving_path, 'final_model_parameters.pth'))
+
     final_paths = []
-    final_paths.append(os.path.join(saving_path, 'final_ambiguity_thresholds_layer_1.p'))
-    final_paths.append(os.path.join(saving_path, 'final_anneal_params_1.p'))
+    for i in range(0,args.number_of_layers*2,2):
+        final_paths.append(os.path.join(saving_path, 'final_ambiguity_thresholds_layer_'+str(i)+'.p'))
+        final_paths.append(os.path.join(saving_path, 'final_anneal_params_'+str(i+1)+'.p'))
     save_model_anneal_params(final_model, final_paths)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs_test, labels_test in test_loader:
+            inputs_test = inputs_test.to(device)
+            labels_test = labels_test.to(device)
+            outputs_test = final_model(inputs_test)
+            _, predicted = torch.max(outputs_test.data, 1)
+            total += labels_test.size(0)
+            correct += (predicted == labels_test).sum().item()
+
+            # print statistics
+        print('Accuracy of the network on the %d test examples: %.2f %%' % (
+        test_loader.dataset.examples.shape[0],
+        100 * correct / total))
+
+    path_to_parameters_save = os.path.join(GetEnvVar('ModelsPath'), 'Guy', 'HTE_pytorch', experiment_name,
+                                        experiment_number, 'final_parameters_final_values.csv')
+    path_to_hyper_parameters_save = os.path.join(GetEnvVar('ModelsPath'), 'Guy', 'HTE_pytorch', experiment_name,
+                                        experiment_number, 'final_hyper_parameters_values.csv')
+    print_end_experiment_report(args, final_model, optimizer,
+                                (100 * correct / total), total,
+                                path_to_parameters_save,
+                                path_to_hyper_parameters_save)
+
 
     test_model = HTE(args, args.input_size, device)
     test_model.load_state_dict(torch.load(os.path.join(saving_path, 'best_model_parameters.pth')), strict=False)
@@ -158,7 +193,14 @@ def main():
         100 * correct / total))
 
     print('Finished Training')
-
+    path_to_parameters_save = os.path.join(GetEnvVar('ModelsPath'), 'Guy', 'HTE_pytorch', experiment_name,
+                                        experiment_number, 'best_parameters_final_values.csv')
+    path_to_hyper_parameters_save = os.path.join(GetEnvVar('ModelsPath'), 'Guy', 'HTE_pytorch', experiment_name,
+                                        experiment_number, 'best_hyper_parameters_values.csv')
+    print_end_experiment_report(args, test_model, optimizer,
+                                (100 * correct / total), total,
+                                path_to_parameters_save,
+                                path_to_hyper_parameters_save)
 
 if __name__ == '__main__':
     main()
