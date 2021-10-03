d2e4f442e0d24e1d919f198bcc4686d0ff70a6d6
farjon <guyfar@post.bgu.ac.il>
2021-10-03 09:57:23 +0300

    regression evaluation added

--- CTE/bin/HTE_experiments/evaluation_function_reg.py
+++ CTE/bin/HTE_experiments/evaluation_function_reg.py
@@ -1,5 +1,8 @@
 import torch
 from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
 
 def eval_loop(test_loader, model, device, args):
     correct = 0
@@ -20,28 +23,23 @@ def eval_loop(test_loader, model, device, args):
                 y_pred.extend(predicted.detach().cpu().numpy().tolist())
                 correct += (predicted == labels_test).sum().item()
 
-    if args.task == 'reg':
-        if args.monitor_mse:
-            ret_score = metrics.mean_squared_error(y_pred, y_true)
-            print(f'the mse is {ret_score}')
-        if args.monitor_mae:
-            ret_score = metrics.mean_absolute_error(y_pred, y_true)
-            print(f'the mae is {ret_score}')
-    elif args.task == 'cls':
-        if args.monitor_acc:
-            accuracy = 100*(correct/test_examples)
-            print(f'the accuracy is {accuracy}')
-            ret_score = accuracy
-        if args.monitor_balanced_acc:
-            balanced_accuracy = metrics.balanced_accuracy_score(y_true, y_pred)
-            print(f'the balanced accuracy is {balanced_accuracy}')
-            ret_score = balanced_accuracy
-        if args.monitor_auc:
-            fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
-            auc_score = metrics.auc(fpr, tpr)
-            print(f'the auc is {auc_score}')
-            ret_score = auc_score
-
+    if args.monitor_acc:
+        accuracy = 100*(correct/test_examples)
+        print(f'the accuracy is {accuracy}')
+        ret_score = accuracy
+    if args.monitor_balanced_acc:
+        balanced_accuracy = metrics.balanced_accuracy_score(y_true, y_pred)
+        print(f'the balanced accuracy is {balanced_accuracy}')
+        ret_score = balanced_accuracy
+    if args.monitor_auc:
+        fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
+        auc_score = metrics.auc(fpr, tpr)
+        print(f'the auc is {auc_score}')
+        ret_score = auc_score
+    if args.monitor_mse:
+        ret_score = mean_squared_error(y_pred, y_true)
+    if args.monitor_mae:
+        ret_score = mean_absolute_error(y_pred, y_true)
     # accuracy = 100*(correct/test_examples)
     # print(f'the accuracy is {accuracy}')
     # ret_score = accuracy
